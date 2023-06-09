88b94aed27a287e899dfdc35008697b47585226b
mashhisyam <haryoh48@gmail.com>
2023-06-09 19:59:21 +0700

    logo login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,13 +5,18 @@
     <!-- Register -->
     <div class="row m-0 p-0">
         <div class="col col-1 col-md-6 d-flex p-0 justify-content-center" id="col-1">
-            <img class="m-0" id="gambar" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F1.bp.blogspot.com%2F-JdbM280Rs2Y%2FXjxgQTF6HEI%2FAAAAAAAAAAg%2FYdyLp9V1tDkp2-kaFXCcgc9IeQNxt5o1gCLcBGAsYHQ%2Fs1600%2Fdummy.jpg&f=1&nofb=1&ipt=3c8f3d2c29ec7f51c9b6b49b45d6f443e5f931da80ca0f88f40bbe02faebe9bd&ipo=images"  style="height:100vh;">
+            <img class="m-0" id="gambar" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F1.bp.blogspot.com%2F-JdbM280Rs2Y%2FXjxgQTF6HEI%2FAAAAAAAAAAg%2FYdyLp9V1tDkp2-kaFXCcgc9IeQNxt5o1gCLcBGAsYHQ%2Fs1600%2Fdummy.jpg&f=1&nofb=1&ipt=3c8f3d2c29ec7f51c9b6b49b45d6f443e5f931da80ca0f88f40bbe02faebe9bd&ipo=images" style="height:100vh;">
         </div>
-        <div id="col-2" class="col col-2 col-md-6 d-flex justify-content-center align-items-center" style="background-color:#2D3F75; border: 2px solid;">
+        <div id="col-2" class="col col-3 col-md-6 d-flex justify-content-center align-items-center" style="background-color:#2D3F75;">
             <div style="width: 500px;">
+                <div class="text-center" style="margin-bottom: 40px;">
+                    <img class="my-4" src="{{ asset('assets/logoPupr.png') }}" alt="">
+                    <h2 class="mb-2 fw-bolder" style="color: #fff; font-size: large;">BALAI TEKNIK RAWA</h1>
+                    <h3 class=" fw-light" style="color: #fff; font-size: large;">Kementrian Pekerjaan Umum dan Perumahan Rakyat</h3>
+                </div>
                 <div class="text-center">
-                    <h1 class="mb-2 fw-bolder" style="color: #fff;">BATARA</h1>
-                    <h3 class=" fw-bolder" style="color: #fff;">Basis Data Rawa</h3>
+                    <h2 class="mb-2 fw-bolder" style="color: #fff;">SANDRA</h1>
+                    <h3 class=" fw-light" style="color: #fff; font-size: large;">Sistem Elektronika Data Rawa</h3>
                 </div>
                 <form id="formAuthentication" class="mb-3" action="{{ url('') }}" method="GET">
                     <div class="mb-3">
